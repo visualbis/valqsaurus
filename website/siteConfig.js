56b97944130eca388d15c2bae960edbd12343796
Praveen N <praveenn@visualbi.com>
2020-01-02 15:53:01 +0530

    circle ci added

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: 'ValQ', // Title for your website.
   tagline: 'Modern Digital Planning',
-  url: 'https://valq.com', // Your website URL
-  baseUrl: '/', // Base URL for your project */
+  url: 'https://visualbis.github.io', // Your website URL
+  baseUrl: '/valqsaurus/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'test-site',
-  organizationName: 'Visual BI',
+  projectName: 'valqsaurus',
+  organizationName: 'visualbis',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
