c6c5b0eae8cf7e7e35740f2881d024197333f798
Srikkanth Mohanasundaram <srikkanthm@visualbi.com>
2020-01-03 15:30:27 +0530

    Update title

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,8 +59,8 @@ function Home() {
   const {siteConfig = {}} = context;
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title} - Modern Digital Planning`}
+      description="ValQ - Modern Digital Planning">
       <header className={classnames('hero hero--primary', styles.heroBanner)}>
         <div className="container">
           <h1 className="hero__title">{siteConfig.title}</h1>
