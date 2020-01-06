1c4a17a3bee3ef54555129d69391255c63f08788
Praveen N <praveenn@visualbi.com>
2020-01-06 14:37:50 +0530

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,88 +1,88 @@
 module.exports = {
-  title: 'ValQ',
-  tagline: 'Modern Digital Planning',
-  url: 'https://valq.com',
-  baseUrl: '/valqsaurus/',
-  favicon: 'img/cropped-valq-icon-1-192x192.png',
-  organizationName: 'visualbis', // Usually your GitHub org/user name.
-  projectName: 'valqsaurus', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: 'ValQ',
-      logo: {
-        alt: 'ValQ Logo',
-        src: 'img/valq-logo-light.png',
-      },
-      links: [
-        {to: 'docs/general/getting-started', label: 'Docs', position: 'right'}
-      ],
+    title: 'ValQ',
+    tagline: 'Modern Digital Planning',
+    url: 'https://valq.com',
+    baseUrl: '/valqsaurus/',
+    favicon: 'img/cropped-valq-icon-1-192x192.png',
+    organizationName: 'visualbis', // Usually your GitHub org/user name.
+    projectName: 'valqsaurus', // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            title: 'ValQ',
+            logo: {
+                alt: 'ValQ Logo',
+                src: 'img/valq-icon.png',
+            },
+            links: [
+                { to: 'docs/general/getting-started', label: 'Docs', position: 'right' }
+            ],
+        },
+        footer: {
+            style: 'light',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Style Guide',
+                            to: 'docs/doc1',
+                        },
+                        {
+                            label: 'Second Doc',
+                            to: 'docs/doc2',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/docusaurus',
+                        },
+                    ],
+                },
+                {
+                    title: 'Social',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: 'blog',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/facebook/docusaurus',
+                        },
+                        {
+                            label: 'Twitter',
+                            href: 'https://twitter.com/docusaurus',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Visual BI`,
+        },
+        algolia: {
+            apiKey: 'api-key',
+            indexName: 'index-name',
+            algoliaOptions: {}, // Optional, if provided by Algolia
+        }
     },
-    footer: {
-      style: 'light',
-      links: [
-        {
-          title: 'Docs',
-          items: [
+    presets: [
+        [
+            '@docusaurus/preset-classic',
             {
-              label: 'Style Guide',
-              to: 'docs/doc1',
+                docs: {
+                    sidebarPath: require.resolve('./sidebars.js')
+                },
+                theme: {
+                    customCss: require.resolve('./src/css/custom.css'),
+                },
             },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'Social',
-          items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Visual BI`,
-    },
-    algolia: {
-      apiKey: 'api-key',
-      indexName: 'index-name',
-      algoliaOptions: {}, // Optional, if provided by Algolia
-    }
-  },
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      {
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js')
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      },
+        ],
     ],
-  ],
 };
--- search-build.js
+++ search-build.js
@@ -2,106 +2,106 @@ const jsdom = require("jsdom");
 const fs = require("fs-extra");
 const path = require("path");
 const cheerio = require("cheerio");
-const { JSDOM } = jsdom;
 const BUILD_PATH = "./build/";
 const SEARCH_DATA = [];
 
 const getContent = element => {
-  return element
-    .text()
-    .replace(/\s\s+/g, " ")
-    .replace(/(\r\n|\n|\r)/gm, " ")
-    .trim();
+    const text = element.is("table") || element.find('table').length !== 0 ?
+        element.html().replace(/<[^>]*>/g, " ") :
+        element.text();
+    return text
+        .replace(/\s\s+/g, " ")
+        .replace(/(\r\n|\n|\r)/gm, " ")
 };
 
-const getSectionContent = function(section) {
-  let content = "";
-  section = section.next();
-  while (section.length) {
-    if (section.is("h2")) break;
-    content += getContent(section) + " ";
+const getSectionContent = function (section) {
+    let content = "";
     section = section.next();
-  }
-  return content;
+    while (section.length) {
+        if (section.is("h2")) break;
+        content += getContent(section) + " ";
+        section = section.next();
+    }
+    return content;
 };
 
 const searchDirectory = (startPath, extension, callback) => {
-  if (!fs.existsSync(startPath)) {
-    return;
-  }
-
-  const files = fs.readdirSync(startPath);
-  files.forEach(file => {
-    const filePath = path.join(startPath, file);
-    const stats = fs.lstatSync(filePath);
-    if (stats.isDirectory()) {
-      searchDirectory(filePath, extension, callback);
-    } else if (path.extname(filePath) === extension) {
-      callback(filePath);
+    if (!fs.existsSync(startPath)) {
+        return;
     }
-  });
+
+    const files = fs.readdirSync(startPath);
+    files.forEach(file => {
+        const filePath = path.join(startPath, file);
+        const stats = fs.lstatSync(filePath);
+        if (stats.isDirectory()) {
+            searchDirectory(filePath, extension, callback);
+        } else if (path.extname(filePath) === extension) {
+            callback(filePath);
+        }
+    });
 };
 
 // Build search data for a html
 const buildSearchData = filePath => {
-  const htmlFile = fs.readFileSync(filePath);
-  //   const dom = new JSDOM(htmlFile);
-  const $ = cheerio.load(htmlFile);
+    const htmlFile = fs.readFileSync(filePath);
+    //   const dom = new JSDOM(htmlFile);
+    const $ = cheerio.load(htmlFile);
 
-  const article = $("article");
-  if (!article.length) {
-    return;
-  }
-  const markdown = article.find(".markdown");
-  if (!markdown.length) {
-    return;
-  }
+    const article = $("article");
+    if (!article.length) {
+        return;
+    }
+    const markdown = article.find(".markdown");
+    if (!markdown.length) {
+        return;
+    }
 
-  let baseUrl = filePath.split(path.sep);
-  // remove build folder path from url
-  baseUrl.splice(0, 1);
-  // remove index.html from the path
-  baseUrl.pop();
-  baseUrl = baseUrl.join("/");
-  const pageTitleElement = article.find("h1");
-  if (!pageTitleElement.length) {
-    console.log(filePath);
-    return;
-  }
-  const pageTitle = article.find("h1").text();
-  const sectionHeaders = markdown.find("h2");
+    let baseUrl = filePath.split(path.sep);
+    // remove build folder path from url
+    baseUrl.splice(0, 1);
+    // remove index.html from the path
+    baseUrl.pop();
+    baseUrl = baseUrl.join("/");
+    const pageTitleElement = article.find("h1");
+    if (!pageTitleElement.length) {
+        console.log(filePath);
+        return;
+    }
+    const pageTitle = article.find("h1").text();
+    const sectionHeaders = markdown.find("h2");
 
-  SEARCH_DATA.push({
-    title: pageTitle,
-    type: 0,
-    sectionRef: "#",
-    url: baseUrl,
-    // If there is no sections then push the complete content under page title
-    content: sectionHeaders.length === 0 ? getContent(markdown) : ""
-  });
-
-  sectionHeaders.map((i, sectionHeader) => {
-    sectionHeader = $(sectionHeader);
-    const title = sectionHeader.text().slice(1);
-    const sectionRef = sectionHeader
-      .children()
-      .first()
-      .attr("id");
-    const url = `${baseUrl}#${sectionRef}`;
-    const content = getSectionContent(sectionHeader);
     SEARCH_DATA.push({
-      title,
-      type: 1,
-      pageTitle,
-      url,
-      content
+        title: pageTitle,
+        type: 0,
+        sectionRef: "#",
+        url: baseUrl,
+        // If there is no sections then push the complete content under page title
+        content: sectionHeaders.length === 0 ? getContent(markdown) : ""
     });
-  });
+
+    sectionHeaders.map((i, sectionHeader) => {
+        sectionHeader = $(sectionHeader);
+        const title = sectionHeader.text().slice(1);
+        const sectionRef = sectionHeader
+            .children()
+            .first()
+            .attr("id");
+        const url = `${baseUrl}#${sectionRef}`;
+        const content = getSectionContent(sectionHeader);
+        SEARCH_DATA.push({
+            title,
+            type: 1,
+            pageTitle,
+            url,
+            content
+        });
+    });
 };
 
 const init = () => {
-  searchDirectory(BUILD_PATH, ".html", buildSearchData);
-  fs.writeJSON("./search-data.json", SEARCH_DATA, { spaces: 2 });
+    searchDirectory(BUILD_PATH, ".html", buildSearchData);
+    fs.writeJSON("./search-data.json", SEARCH_DATA, { spaces: 2 });
 };
 
 init();
