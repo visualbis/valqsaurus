f1e1e5f15d497f57056d6bdd79380ba0116bad5b
Srikkanth Mohanasundaram <srikkanthm@visualbi.com>
2020-01-06 14:42:11 +0530

    Fix merge issue

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,71 +1,72 @@
 module.exports = {
-    title: 'ValQ',
-    tagline: 'Modern Digital Planning',
-    url: 'https://valq.com',
-    baseUrl: '/valqsaurus/',
-    favicon: 'img/cropped-valq-icon-1-192x192.png',
-    organizationName: 'visualbis', // Usually your GitHub org/user name.
-    projectName: 'valqsaurus', // Usually your repo name.
-    themeConfig: {
-        navbar: {
-            title: 'ValQ',
-            logo: {
-                alt: 'ValQ Logo',
-                src: 'img/valq-icon.png',
-            },
-            links: [
-                { to: 'docs/general/getting-started', label: 'Docs', position: 'right' }
-            ],
-        },
-        footer: {
-            style: 'light',
-            links: [
-                {
-                    title: 'Docs',
-                    items: [
-                        {
-                            label: 'Documentation',
-                            to: 'docs/general/getting-started',
-                        }
-                    ],
-                },
-                {
-                    title: 'Community',
-                    items: [
-                        {
-                            label: 'ValQ Community',
-                            href: 'https://community.valq.com/',
-                        }
-                    ],
-                },
-                {
-                    title: 'Social',
-                    items: [
-                        {
-                            label: 'Blog',
-                            href: 'https://valq.com/blogs/',
-                        }
-                    ],
-                },
-            ],
-            copyright: `Copyright © ${new Date().getFullYear()} Visual BI`,
-        },
-        algolia: {
-            apiKey: 'api-key',
-            indexName: 'index-name',
-            algoliaOptions: {}, // Optional, if provided by Algolia
-        }
+  title: "ValQ",
+  tagline: "Modern Digital Planning",
+  url: "https://valq.com",
+  baseUrl: "/valqsaurus/",
+  favicon: "img/cropped-valq-icon-1-192x192.png",
+  organizationName: "visualbis", // Usually your GitHub org/user name.
+  projectName: "valqsaurus", // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: "ValQ",
+      logo: {
+        alt: "ValQ Logo",
+        src: "img/valq-icon.png"
+      },
+      links: [
+        { to: "docs/general/getting-started", label: "Docs", position: "right" }
+      ]
     },
-    presets: [
-        [
-            '@docusaurus/preset-classic',
+    footer: {
+      style: "light",
+      links: [
+        {
+          title: "Docs",
+          items: [
             {
-                docs: {
-                    sidebarPath: require.resolve('./sidebars.js')
-                },
-                theme: {
-                    customCss: require.resolve('./src/css/custom.css'),
-                },
-            },
-        ],
+              label: "Documentation",
+              to: "docs/general/getting-started"
+            }
+          ]
+        },
+        {
+          title: "Community",
+          items: [
+            {
+              label: "ValQ Community",
+              href: "https://community.valq.com/"
+            }
+          ]
+        },
+        {
+          title: "Social",
+          items: [
+            {
+              label: "Blog",
+              href: "https://valq.com/blogs/"
+            }
+          ]
+        }
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Visual BI`
+    },
+    algolia: {
+      apiKey: "api-key",
+      indexName: "index-name",
+      algoliaOptions: {} // Optional, if provided by Algolia
+    }
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js")
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css")
+        }
+      }
+    ]
+  ]
 };
