6507d32688583ab6229833cd0a33d7160bf9933f
divya bhushan <divya_bhushan@hotmail.com>
2020-11-23 16:18:37 +0100

    Adding the missing excel downloadable files in the how to videos section

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,90 +1,82 @@
 module.exports = {
-  title: "ValQ",
-  tagline: "Modern Digital Planning",
-  url: "https://valq.com",
-  baseUrl: "/",
-  favicon: "img/cropped-valq-icon-1-192x192.png",
-  organizationName: process.env.CIRCLE_PROJECT_USERNAME || "visualbis", // Usually your GitHub org/user name.
-  projectName: "valqsaurus", // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      hideOnScroll: true,
-      title: "ValQ",
-      logo: {
-        alt: "ValQ Logo",
-        src: "img/valq-icon.png",
-      },
-      items: [
-        {
-          to: "docs/intro/introduction",
-          label: "Docs",
-          position: "right",
+    title: "ValQ",
+    tagline: "Modern Digital Planning",
+    url: "https://valq.com",
+    baseUrl: "/",
+    favicon: "img/cropped-valq-icon-1-192x192.png",
+    organizationName: process.env.CIRCLE_PROJECT_USERNAME || "visualbis", // Usually your GitHub org/user name.
+    projectName: "valqsaurus", // Usually your repo name.
+    themeConfig: {
+        navbar: {
+            hideOnScroll: true,
+            title: "ValQ",
+            logo: {
+                alt: "ValQ Logo",
+                src: "img/valq-icon.png",
+            },
+            items: [{
+                to: "docs/intro/introduction",
+                label: "Docs",
+                position: "right",
+            }, ],
         },
-      ],
+        footer: {
+            style: "light",
+            links: [{
+                    title: "Docs",
+                    items: [{
+                            label: "Documentation",
+                            to: "docs/intro/introduction",
+                        },
+                        {
+                            label: "Functions",
+                            to: "docs/references/formulas/if",
+                        },
+                    ],
+                },
+                {
+                    title: "More",
+                    items: [{
+                            label: "ValQ Community",
+                            href: "https://community.valq.com/",
+                        },
+                        {
+                            label: "Status",
+                            href: "http://status.valq.com/",
+                        },
+                    ],
+                },
+                {
+                    title: "Social",
+                    items: [{
+                        label: "Blog",
+                        href: "https://valq.com/blogs/",
+                    }, ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Visual BI`,
+        },
+        algolia: {
+            apiKey: "api-key",
+            indexName: "index-name",
+            algoliaOptions: {}, // Optional, if provided by Algolia
+            contextualSearch: true,
+        },
+        ogImage: "img/whats-new-in-valq.jpg",
+        twitterImage: "img/whats-new-in-valq.jpg",
     },
-    footer: {
-      style: "light",
-      links: [
-        {
-          title: "Docs",
-          items: [
+    plugins: ["docusaurus-lunr-search"],
+    presets: [
+        [
+            "@docusaurus/preset-classic",
             {
-              label: "Documentation",
-              to: "docs/intro/introduction",
+                docs: {
+                    sidebarPath: require.resolve("./sidebars.js"),
+                },
+                theme: {
+                    customCss: require.resolve("./src/css/custom.css"),
+                },
             },
-            {
-              label: "Functions",
-              to: "docs/references/formulas/if",
-            },
-          ],
-        },
-        {
-          title: "More",
-          items: [
-            {
-              label: "ValQ Community",
-              href: "https://community.valq.com/",
-            },
-            {
-              label: "Status",
-              href: "http://status.valq.com/",
-            },
-          ],
-        },
-        {
-          title: "Social",
-          items: [
-            {
-              label: "Blog",
-              href: "https://valq.com/blogs/",
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Visual BI`,
-    },
-    algolia: {
-      apiKey: "api-key",
-      indexName: "index-name",
-      algoliaOptions: {}, // Optional, if provided by Algolia
-      contextualSearch: true,
-    },
-    ogImage: "img/whats-new-in-valq.jpg",
-    twitterImage: "img/whats-new-in-valq.jpg",
-  },
-  onBrokenLinks: "ignore",
-  plugins: ["docusaurus-lunr-search"],
-  presets: [
-    [
-      "@docusaurus/preset-classic",
-      {
-        docs: {
-          sidebarPath: require.resolve("./sidebars.js"),
-        },
-        theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-        },
-      },
+        ],
     ],
-  ],
 };
