5ec5ea780600b37bd05cd9e5d22b89e3ac658030
divya bhushan <divya_bhushan@hotmail.com>
2020-12-01 17:55:37 +0100

    Updated lunr search to v2.1.0.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,54 +18,48 @@ module.exports = {
                 to: "docs/intro/introduction",
                 label: "Docs",
                 position: "right",
-            }, ],
+            },],
         },
         footer: {
             style: "light",
             links: [{
-                    title: "Docs",
-                    items: [{
-                            label: "Documentation",
-                            to: "docs/intro/introduction",
-                        },
-                        {
-                            label: "Functions",
-                            to: "docs/references/formulas/if",
-                        },
-                    ],
+                title: "Docs",
+                items: [{
+                    label: "Documentation",
+                    to: "docs/intro/introduction",
                 },
                 {
-                    title: "More",
-                    items: [{
-                            label: "ValQ Community",
-                            href: "https://community.valq.com/",
-                        },
-                        {
-                            label: "Status",
-                            href: "http://status.valq.com/",
-                        },
-                    ],
+                    label: "Functions",
+                    to: "docs/references/formulas/if",
+                },
+                ],
+            },
+            {
+                title: "More",
+                items: [{
+                    label: "ValQ Community",
+                    href: "https://community.valq.com/",
                 },
                 {
-                    title: "Social",
-                    items: [{
-                        label: "Blog",
-                        href: "https://valq.com/blogs/",
-                    }, ],
+                    label: "Status",
+                    href: "http://status.valq.com/",
                 },
+                ],
+            },
+            {
+                title: "Social",
+                items: [{
+                    label: "Blog",
+                    href: "https://valq.com/blogs/",
+                },],
+            },
             ],
             copyright: `Copyright © ${new Date().getFullYear()} Visual BI`,
-        },
-        algolia: {
-            apiKey: "api-key",
-            indexName: "index-name",
-            algoliaOptions: {}, // Optional, if provided by Algolia
-            contextualSearch: true,
         },
         ogImage: "img/whats-new-in-valq.jpg",
         twitterImage: "img/whats-new-in-valq.jpg",
     },
-    plugins: ["docusaurus-lunr-search"],
+    plugins: [require.resolve('docusaurus-lunr-search')],
     presets: [
         [
             "@docusaurus/preset-classic",
