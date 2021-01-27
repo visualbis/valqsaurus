f2196a34253c9db7f032e4341aabb9e81e1dd0aa
divyabhushan <divya_bhushan@hotmail.com>
2021-01-27 12:41:10 +0100

    Restyling product tag header1 with docusaurus h1 class

--- src/ProductTag.js
+++ src/ProductTag.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./css/custom.css";
+import "react-medium-image-zoom/dist/styles.css";
 
 export const Tag = (props) => {
     return (
         <div>
             <header>
-                <h1 class="docTitle_1Lrw">{props.header} <div><img src={props.image} /></div></h1>
+                <h1 class="docTitle_node_modules-@docusaurus-theme-classic-lib-theme-DocItem-">
+                    {props.header}<div id="tag"><img src={props.image} /></div>
+                </h1>
             </header>
         </div>
     );
 };
+
+
