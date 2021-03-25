6bd50c1bc31eb9c1c3583b0245c700774589f959
divyabhushan <divya_bhushan@hotmail.com>
2021-03-25 14:22:07 +0100

    ValQ workspace addition

--- sidebars.js
+++ sidebars.js
@@ -9,6 +9,8 @@ module.exports = {
     sideBar: {
         Introduction: [
             "intro/introduction",
+            "intro/releases",
+            "intro/valq-workspace",
             "intro/install",
             "intro/model",
             "intro/node",
